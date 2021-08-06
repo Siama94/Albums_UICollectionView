a88608f22aef419f6e550b3dc1ba9b9997d6ca9d
Anastasiia Maksimova <sia.m94@ya.ru>
2021-08-06 20:39:49 +0500

    Add UICollectionView.

--- Albums_UICollectionView/ViewControllers/ThirdViewController.swift
+++ Albums_UICollectionView/ViewControllers/ThirdViewController.swift
@@ -14,14 +14,68 @@ class ThirdViewController: UIViewController {
     @objc func leftButtonItem(param: UIBarButtonItem) {
     }
     
+    private let collectionView: UICollectionView = {
+        let viewLayout = UICollectionViewFlowLayout()
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: viewLayout)
+        collectionView.backgroundColor = .white
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        return collectionView
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.reloadData()
         view.backgroundColor = .white
         self.title = "Альбомы"
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
                                                                  target: self,
                                                                  action: #selector(leftButtonItem(param:)))
     }
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) - fatalError")
+    }
+    
+    // MARK: - Settings
+    
+    private func setupLayouts() {
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            collectionView.rightAnchor.constraint(equalTo: view.rightAnchor)
+        ])
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .white
+        view.addSubview(collectionView)
+    }
+}
+
+extension FirstViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+      
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        return cell
+        }
 }

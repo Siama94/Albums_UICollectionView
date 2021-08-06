8546e17f3e9c8d0333ad1a5d12ae0be57b834b17
Anastasiia Maksimova <sia.m94@ya.ru>
2021-08-06 20:29:35 +0500

    Create navigation and tabBar.

--- Albums_UICollectionView/AppDelegate.swift
+++ Albums_UICollectionView/AppDelegate.swift
@@ -10,27 +10,40 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let tabBarController = UITabBarController()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let firstViewController = FirstViewController()
+        firstViewController.tabBarItem = UITabBarItem(title: "Медиатека", image: UIImage(systemName: "photo.fill.on.rectangle.fill"), tag: 0)
+        
+        let secondViewController = SecondViewController()
+        secondViewController.tabBarItem = UITabBarItem(title: "Для Вас", image: UIImage(systemName: "heart.text.square.fill"), tag: 1)
+
+        let thirdViewController = ThirdViewController()
+        let thirdNavigationController = UINavigationController(rootViewController: thirdViewController)
+        thirdViewController.tabBarItem = UITabBarItem(title: "Альбомы", image: UIImage(systemName: "rectangle.stack.fill"), tag: 2)
+        thirdNavigationController.navigationBar.prefersLargeTitles = true
+        thirdNavigationController.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        thirdNavigationController.navigationBar.shadowImage = UIImage()
+        thirdNavigationController.navigationBar.isTranslucent = true
+        thirdNavigationController.view.backgroundColor = .clear
+        
+        let fourViewController = FourViewController()
+        fourViewController.tabBarItem = UITabBarItem(title: "Поиск", image: UIImage(systemName: "magnifyingglass"), tag: 3)
+
+        tabBarController.setViewControllers([firstViewController,
+                                             secondViewController,
+                                             thirdNavigationController,
+                                             fourViewController],
+                                            animated: true)
+        
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
-

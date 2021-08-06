6c33ccdd338714ad478d914d388f8e5f7ac966e0
Anastasiia Maksimova <sia.m94@ya.ru>
2021-08-06 20:04:49 +0500

    Initial Commit

---
+++ Albums_UICollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Albums_UICollectionView
+//
+//  Created by Анастасия on 06.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
